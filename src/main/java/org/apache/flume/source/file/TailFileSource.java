47206d192d230507b072d8bc0b6f692eb70b8763
AndrewYJ <Andrewyj@163.com>
2014-11-23 15:57:43 +0800

    create TailFileSource class

---
+++ src/main/java/org/apache/flume/source/file/TailFileSource.java
@@ -0,0 +1,17 @@
+package org.apache.flume.source.file;
+
+import org.apache.flume.Context;
+import org.apache.flume.EventDrivenSource;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.source.AbstractSource;
+
+public class TailFileSource extends AbstractSource implements Configurable,EventDrivenSource{
+
+
+	@Override
+	public void configure(Context arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
